ea649640c97e24c5ed9d1a9135e3364cd4eeec8a
hsaleem7 <89563278+hsaleem7@users.noreply.github.com>
2021-08-25 23:28:56 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/**********************************************************
+* Hello
+***********************************************************
+* Hiba Saleem
+* August 25, 2021
+* CMSC 255 C90
+***********************************************************/
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("Hiba Saleem");
+		System.out.println("Hello");
+		System.out.println("Lab 1");
+		System.out.println("August 25, 2021");
+		System.out.println("CMSC 255 C90");
+		System.out.println();
+	}
+}
